f993aa8853846847915e4fc580d140741f6c1ffe
Martin Michelsen <mjem@wildblue.net>
2013-09-01 11:26:48 -0700

    display: fix interrupt behavior

--- display.c
+++ display.c
@@ -12,11 +12,12 @@ void display_update(struct display* d, int cycles) {
 
   if (d->ly == d->lyc) {
     d->status |= 0x04;
-    signal_interrupt(d->cpu, INTERRUPT_LCDSTAT, 1);
+    if (d->status & 0x40)
+      signal_interrupt(d->cpu, INTERRUPT_LCDSTAT, 1);
   } else
     d->status &= ~0x04;
 
-  if (d->ly == 144)
+  if ((d->status & 0x10) && (d->ly == 144))
     signal_interrupt(d->cpu, INTERRUPT_VBLANK, 1);
 }
 
@@ -30,6 +31,10 @@ void write_lcd_reg(struct display* d, uint8_t addr, uint8_t value) {
     case 0x41:
       // can't write the lower 3 bits
       d->status = (d->status & 7) | (value & 0x78);
+      if (d->status & 0x20)
+        printf("warning: lcd oam interrupt enabled but not implemented\n");
+      if (d->status & 0x08)
+        printf("warning: lcd hblank interrupt enabled but not implemented\n");
       break;
     case 0x46:
       // TODO: DMA that shit
