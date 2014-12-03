2c12130be9613bfc3034e39cdf7431b5c3724c16
Martin Michelsen <mjem@wildblue.net>
2014-12-02 18:52:18 -0800

    add bounds checking for audio regs

--- audio.c
+++ audio.c
@@ -16,9 +16,13 @@ void audio_update(struct audio* a, int cycles) {
 }
 
 uint8_t read_audio_register(struct audio* a, uint8_t addr) {
+  if (addr < 0x10 || addr > 0x3F)
+    signal_debug_interrupt(a->cpu, "invalid audio reg read");
   return ((uint8_t*)a)[addr - 0x10];
 }
 
 void write_audio_register(struct audio* a, uint8_t addr, uint8_t value) {
+  if (addr < 0x10 || addr > 0x3F)
+    signal_debug_interrupt(a->cpu, "invalid audio reg write");
   ((uint8_t*)a)[addr - 0x10] = value;
 }
